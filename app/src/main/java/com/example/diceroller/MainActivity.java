d545ab5f41dcb07433027214af9ed6490a212e7a
Meshal <1809911@brunel.ac.uk>
2019-11-08 04:10:43 +0000

    (could not push for Part IIIa so had to commit)

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -8,17 +8,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.TextureView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int correct_counter = 0;
+
+    public static int Roll_the_dice() {
+        Random r = new Random();
+        return r.nextInt(6);
+    }
 
 
     @Override
@@ -81,5 +86,41 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+    public void on_click_icebreaker(View view) {
+        int Roll = Roll_the_dice();
 
-}
+        TextView tv = this.findViewById(R.id.textView5);
+        List<String> Questions = new ArrayList<>();// Questions arraylist
+
+        Questions.add("If you could go anywhere in the world, where would you go?"); //1
+        Questions.add("If you were stranded on a desert island, what three things would you want to take with you?"); //2
+        Questions.add("If you could eat only one food for the rest of your life, what would that be?"); //3
+        Questions.add("If you won a million dollars, what is the first thing you would buy?"); //4
+        Questions.add("If you could spaned the day with one fictional character, who would it be?"); //5
+        Questions.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?"); //6
+
+        if (Roll == 1) // Question 1
+            tv.setText(Questions.get(0));
+
+        if (Roll == 2) // Question 2
+            tv.setText(Questions.get(1));
+
+        if (Roll == 3) // Question 3
+            tv.setText(Questions.get(2));
+
+        if (Roll == 4) // Question 4
+            tv.setText(Questions.get(3));
+
+        if (Roll == 5) // Question 5
+            tv.setText(Questions.get(4));
+
+        if (Roll == 6) // Question 6
+            tv.setText(Questions.get(5));
+
+
+
+    }
+
+    }
+
+
